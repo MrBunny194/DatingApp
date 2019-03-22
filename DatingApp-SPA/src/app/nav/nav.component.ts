86db4681fbeeb1ec27a303f0cb1d3916d53afdf9
NikitaVolobuev <nikita.18@icloud.com>
2019-03-23 00:39:45 +0400

    End of section 6

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,36 +1,36 @@
-import { Component, OnInit } from "@angular/core";
-import { AuthService } from "../_services/auth.service";
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
-  selector: "app-nav",
-  templateUrl: "nav.component.html",
-  styleUrls: ["./nav.component.css"]
+  selector: 'app-nav',
+  templateUrl: 'nav.component.html',
+  styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(private authService: AuthService) {}
+  constructor(public authService: AuthService, private alertify: AlertifyService) { }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
   login() {
     this.authService.login(this.model).subscribe(
       next => {
-        console.log("Logged in successfully");
+        this.alertify.success('Logged in successfully');
       },
       error => {
-        console.log("Faild to login");
+        this.alertify.error('Faild to login');
       }
     );
   }
 
   loggedIn() {
-    const token = localStorage.getItem('token');
-    return !!token;
+    return this.authService.loggedIn();
   }
 
   logOut() {
-    localStorage.removeItem("token");
-    console.log('logged out')
+    localStorage.removeItem('token');
+    this.alertify.message('logged out');
   }
 }
